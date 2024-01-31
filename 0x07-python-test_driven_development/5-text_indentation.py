84604287360c646128f44970d2063dd47a7dafd8
Laylor <anelamade@gmail.com>
2024-01-31 08:38:13 -0800

    My 2024 commits

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A function that prints a text with 2 new lines after each of these
+characters: ., ? and :
+"""
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines after each ".", "?", or ":"
+    """
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
